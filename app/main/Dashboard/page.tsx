94cc6309f8b25db206634bc3ea77bc294d0b6243
Đỗ Minh Đức <minhducdo202@gmail.com>
2025-04-29 17:22:24 +0700

    second commit

--- app/main/Dashboard/page.tsx
+++ app/main/Dashboard/page.tsx
@@ -1,31 +1,106 @@
 "use client";
 
-import DashboardData from "@/app/component/Dashboard/DashboardData";
-import DashboardInfo from "@/app/component/Dashboard/DashboardInfo";
-
-export interface DashboardProps {
-  gridCols?: string;
-  days?: number[];
-  currentMonth?: number;
-  currentYear?: number;
-}
+import { dataCheckIn } from "@/app/TestData/Dashboard/data";
+import { Table } from "antd";
+import Search from "antd/es/input/Search";
+import { ChangeEvent, useState } from "react";
+import dayjs from "dayjs";
+import "@ant-design/v5-patch-for-react-19";
 
 export default function Dashboard() {
+  const [dataCheck, setDataCheck] = useState(dataCheckIn);
+  const [searchInput, setSearchInput] = useState("");
   const currentMonth = new Date().getMonth() + 1;
   const currentYear = new Date().getFullYear();
 
   const daysInMonth = new Date(currentYear, currentMonth, 0).getDate();
   const days = Array.from({ length: daysInMonth }, (_, index) => index + 1);
-  const gridCols = { 28: "grid-cols-28", 29: "grid-cols-29", 30: "grid-cols-30", 31: "grid-cols-31" }[daysInMonth] || "grid-cols-31";
+
+  const dayColumns = days.map((day, index) => ({
+    title: dayjs(`${currentYear}-${currentMonth}-${day}`).format("DD/MM"),
+    key: `day_${day}`,
+    width: 70,
+    align: "center" as const,
+    render: (_: any, record: any) => {
+      const value = record.employee_check[index];
+      return <span className={`font-semibold ${value > 0 ? "text-green-600" : "text-red-600"}`}>{value}</span>;
+    },
+  }));
+
+  const columns = [
+    {
+      title: "STT",
+      key: "id",
+      align: "center" as const,
+      width: 60,
+      fixed: "left" as const,
+      render: (_: unknown, __: unknown, index: number) => index + 1,
+    },
+    {
+      title: "Mã NV",
+      dataIndex: "employee_code",
+      key: "employee_code",
+      width: 100,
+      fixed: "left" as const,
+      align: "center" as const,
+    },
+    {
+      title: "Họ và tên",
+      dataIndex: "employee_name",
+      key: "employee_name",
+      width: 150,
+      fixed: "left" as const,
+      align: "center" as const,
+    },
+    {
+      title: "Tổng công",
+      key: "total_check",
+      width: 150,
+      fixed: "left" as const,
+      align: "center" as const,
+      render: (_: any, record: any) => {
+        const totalHour = record.employee_check.reduce((sum: number, val: number) => sum + val, 0);
+        const totalCheck = record.employee_check.filter((check: number) => check > 0).length;
+        return (
+          <div className="flex justify-between">
+            <div>{totalHour} giờ</div>
+            <div className="w-[1px] bg-black"></div>
+            <div>{totalCheck} ngày</div>
+          </div>
+        );
+      },
+    },
+    ...dayColumns,
+  ];
+
+  const searchChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearch = (value: string) => {
+    if (!value) {
+      setDataCheck(dataCheckIn);
+    } else {
+      setSearchInput(value);
+      const searchTerm = value.toLowerCase();
+      const filteredData = dataCheckIn.filter((data) => data.employee_name.toLowerCase().includes(searchTerm));
+      setDataCheck(filteredData);
+    }
+  };
 
   return (
     <>
       <div className="flex justify-center text-2xl font-semibold my-3">Thống kê chấm công</div>
-      <div className="overflow-x-auto h-[90%]">
-        <div className="min-w-[1200px] grid grid-cols-[500px_120%]">
-          <DashboardInfo />
-          <DashboardData gridCols={gridCols} days={days} currentMonth={currentMonth} currentYear={currentYear} />
-        </div>
+      <div>
+        <Search
+          placeholder="Tìm kiếm"
+          style={{ width: "20%", marginBottom: 12 }}
+          value={searchInput}
+          onChange={searchChange}
+          onSearch={() => handleSearch(searchInput)}
+          enterButton
+        />
+        <Table dataSource={dataCheck} columns={columns} scroll={{ x: "max-content", y: 55 * 9 }} pagination={false} />
       </div>
     </>
   );
--- app/main/Setting/page.tsx
+++ app/main/Setting/page.tsx
@@ -1,5 +1,103 @@
 "use client";
 
+import PersonIcon from "@mui/icons-material/Person";
+import { Avatar, Button, DatePicker, Input } from "antd";
+import "@ant-design/v5-patch-for-react-19";
+import { ChangeEvent, useState } from "react";
+import dayjs from "dayjs";
+
 export default function Setting() {
-  return <h1>Setting page</h1>;
+  const [formData, setFormData] = useState({
+    employee_phone_number: "",
+    employee_birthday: "",
+    employee_citizen_id: "",
+    employee_bank_account: "",
+    employee_personal_email: "",
+    employee_license_plate: "",
+  });
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleInputNumberChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    const onlyNumbers = value.replace(/[^0-9]/g, "");
+    setFormData((prev) => ({ ...prev, [name]: onlyNumbers }));
+  };
+
+  const handleDateChange = (date: dayjs.Dayjs) => {
+    setFormData((prev) => ({
+      ...prev,
+      employee_birthday: date ? date.format("YYYY-MM-DD") : "",
+    }));
+  };
+
+  return (
+    <>
+      <div className="flex justify-center text-2xl font-semibold my-3">Cài đặt tài khoản</div>
+      <div className="h-[90%] text-center bg-blue-200 rounded-xl">
+        <Avatar size={120} icon={<PersonIcon />} />
+        <div className="flex flex-col-2 justify-center gap-x-10 my-5">
+          <div className="w-[30%] flex flex-col gap-y-5">
+            <Input name="employee_code" placeholder="Mã nhân viên" readOnly />
+            <Input name="employee_name" placeholder="Tên nhân viên" readOnly />
+            <Input name="employee_department" placeholder="Phòng ban" readOnly />
+            <Input name="employee_role" placeholder="Chức vụ" readOnly />
+            <Input name="employee_company_email" placeholder="Email công ty" readOnly />
+          </div>
+          <div className="w-[30%] flex flex-col gap-y-5">
+            <Input
+              name="employee_phone_number"
+              placeholder="Số điện thoại"
+              maxLength={10}
+              value={formData.employee_phone_number}
+              onChange={handleInputNumberChange}
+            />
+            <DatePicker
+              name="employee_birthday"
+              placeholder="Ngày sinh"
+              value={formData?.employee_birthday ? dayjs(formData.employee_birthday) : null}
+              onChange={handleDateChange}
+            />
+            <Input
+              name="employee_citizen_id"
+              placeholder="CCCD/CMND"
+              maxLength={12}
+              value={formData.employee_citizen_id}
+              onChange={handleInputNumberChange}
+            />
+            <Input
+              name="employee_bank_account"
+              placeholder="Số tài khoản TCB"
+              value={formData.employee_bank_account}
+              onChange={handleInputNumberChange}
+            />
+            <Input
+              name="employee_personal_email"
+              placeholder="Email cá nhân"
+              value={formData.employee_personal_email}
+              onChange={handleInputChange}
+            />
+            <Input
+              name="employee_license_plate"
+              placeholder="Biển số xe"
+              maxLength={12}
+              value={formData.employee_license_plate}
+              onChange={handleInputChange}
+            />
+          </div>
+        </div>
+        <div>
+          <Button color="cyan" variant="solid">
+            Cập nhật
+          </Button>
+          <Button type="primary" danger>
+            Đăng xuất
+          </Button>
+        </div>
+      </div>
+    </>
+  );
 }
